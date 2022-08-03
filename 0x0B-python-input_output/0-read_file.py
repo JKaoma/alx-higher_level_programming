c6b2d0fbe15be38a5d3b9df04283219885d5bfe1
JKaoma <james.kaoma@outlook.com>
2022-08-03 03:32:51 +0200

    added files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # 0-read_file.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines a text file-reading function."""
-
 
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
