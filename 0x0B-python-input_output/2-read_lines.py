98161d5643bfd1a46f4a38a81d7af0c8db6c9444
JKaoma <james.kaoma@outlook.com>
2022-08-03 03:32:02 +0200

    added files

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding='utf-8') as f:
-        text = f.readlines()
-        tot_lines = len(text)
+# 2-read_lines.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a text file-reading function."""
 
-        if nb_lines <= 0 or nb_lines >= tot_lines:
-            f.seek(0)
-            print(f.read(), end='')
+
+def read_lines(filename="", nb_lines=0):
+    """Print a given number of lines from a UTF8 text file to stdout.
+
+    Args:
+        filename (str): The name of the file.
+        nb_lines (int): The number of lines to read from the file.
+    """
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+            return
+
+        lines = 0
+        for line in f:
+            lines += 1
+        f.seek(0)
+        if nb_lines >= lines:
+            print(f.read(), end="")
+            return
+
         else:
-            for i in range(nb_lines):
-                print(text[i], end='')
+            n = 0
+            while n < nb_lines:
+                print(f.readline(), end="")
+                n += 1
